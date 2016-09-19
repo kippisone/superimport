b24d3635068c42a69b159a44bee5bcfbdcfbd6f5
Andifeind <andifeind@noname-media.com>
2016-09-19 23:17:12 +0200

    importAll method

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 let path = require('path');
+let fs = require('fs');
 
 let getPaths = function() {
   let curDir = process.cwd();
@@ -16,6 +17,24 @@ let getPaths = function() {
   }
 }
 
+let readDir = function(dir) {
+  let files = [];
+  fs.readdirSync(dir).forEach(file => {
+    let filePath = path.join(dir, file);
+    let stat = fs.statSync(filePath);
+    if (stat.isDirectory()) {
+      files = files.concat(readDir(filePath));
+    }
+    else {
+      if (/\.js(on)?$/.test(file)) {
+        files.push(filePath);
+      }
+    }
+  });
+
+  return files;
+};
+
 module.exports = function(moduleName, paths) {
   paths = paths || module.parent.paths.concat(getPaths());
   for (let dir of paths) {
@@ -27,4 +46,19 @@ module.exports = function(moduleName, paths) {
   }
 
   throw new Error(`Module ${moduleName} not found!\nYou can install it by using the command 'npm install ${moduleName} --save'\n\n`);
+};
+
+module.exports.importAll = function(dir, recursive) {
+  let files = readDir(dir).map(f => require(f));
+  console.log(files, dir)
+  files.apply = function(ctx, args) {
+    files.forEach(file => file.apply(ctx, args));
+  };
+
+  files.call = function(ctx) {
+    let args = Array.prototype.slice.call(arguments, 1);
+    files.apply(ctx, args);
+  };
+
+  return files;
 };
