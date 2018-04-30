026775f0c84e9b4af3cd5c746efe939459092d67
Andifeind <andifeind@noname-media.com>
2018-04-30 23:30:36 +0200

    Update dependencies, add gitlab ci file

--- tests/superimport.spec.js
+++ tests/superimport.spec.js
@@ -50,7 +50,8 @@ describe('superimport', () => {
       inspect(modules).isArray()
       inspect(modules).hasLength(2)
       inspect(modules).hasAnyValues([{
-        name: 'bar'
+        name: 'bar',
+        filename: path.join(__dirname, './modules/sub/bar.js')
       }])
     })
 
@@ -59,9 +60,11 @@ describe('superimport', () => {
       inspect(modules).isArray()
       inspect(modules).hasLength(4)
       inspect(modules).hasAnyValues([{
-        name: 'bar'
+        name: 'bar',
+        filename: path.join(__dirname, './modules/sub/bar.js')
       }, {
-        name: 'blub'
+        name: 'blub',
+        filename: path.join(__dirname, './modules/sub/blub/blub.js')
       }])
     })
   })
